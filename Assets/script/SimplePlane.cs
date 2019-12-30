0ea30a3f5ea325c32c8f3b7e11c4aed971c80ea9
Elbarge <sikodaikonntorabasu@gmail.com>
2019-12-30 15:13:58 +0900

    飛行機作成

---
+++ Assets/script/SimplePlane.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlane : MonoBehaviour
+{
+    public float target_kmph_ = 100f;
+    void FixedUpdate()
+    {
+        var hori = Input.GetAxis("Horizontal") * 3f;
+        var vart = Input.GetAxis("Vertical") * 1f;
+        var rb = GetComponent<Rigidbody>();
+        // AddTorqueはワールド座標
+        // AddRelativeTorqueはローカル座標
+        rb.AddRelativeTorque(new Vector3(0, hori, -hori));
+        rb.AddRelativeTorque(new Vector3(vart, 0, 0));
+
+        var left = transform.TransformVector(Vector3.left);
+        var horizontal_left = new Vector3(left.x, 0f, left.z).normalized;
+        // Crossは外積
+        rb.AddTorque(Vector3.Cross(left, horizontal_left) * 4f);
+        var forward = transform.TransformVector(Vector3.left);
+        var horizontal_forward = new Vector3(left.x, 0f, left.z).normalized;
+        rb.AddTorque(Vector3.Cross(forward, horizontal_forward) * 4f);
+
+        var force = (rb.mass * rb.drag * target_kmph_ / 3.6f) / (1f - rb.drag*Time.fixedDeltaTime);
+        rb.AddRelativeForce(new Vector3(0f, 0f, force));
+    }
+}
---
+++ Assets/script/SimplePlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlayer : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    /* FixedUpdateの説明
+     * Updateは１秒間に何回呼ばれるかわからない（端末性能に左右される）
+     * Time.deltaTimeは変動する（同フレーム内で同値になる）
+     * 
+     * FiexdUpdeteは１秒間に呼ばれる回数が決まっている
+     * Time.fixedDeltaTimeも値が固定
+     * 上記２つを使うことにより正確な値（ゲームバランスの構築ができる）
+    */
+    void FixedUpdate()
+    {
+        var hori = Input.GetAxis("Horizontal");// * Time.fixedDeltaTime
+        var vert = Input.GetAxis("Vertical");// * Time.fixedDeltaTime
+        var force = new Vector3(hori,vert,0f);
+        // transform.position += new Vector3(hori,vert,0f);
+        // Rigidbodyの中でTime.fixedDeltaTimeが使用されているらしい
+        GetComponent<Rigidbody>().AddForce(force * 100f);
+    }
+}
